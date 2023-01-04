9f006a24550b018fa6f334e1a7333d650d5d0413
Claudio <claudio03savarino@gmail.com>
2023-01-04 10:57:51 +0100

    primo commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            for (int j = 0; j < 10; j++) {
+                System.out.println(i);
+            }
+            if (i>=5)
+                break;
+        }
+    }
+}
